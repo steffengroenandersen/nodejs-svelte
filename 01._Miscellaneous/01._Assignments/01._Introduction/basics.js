90f8d901516bf54a7ad7d330bc9b9b68b42bd753
steffengroenandersen <steffengroenandersen@gmail.com>
2024-02-02 10:06:43 +0100

    Add basics.js

---
+++ 01._Miscellaneous/01._Assignments/01._Introduction/basics.js
@@ -0,0 +1,25 @@
+// --------------------------------------
+// Variables, strings, numbers, floats
+// --------------------------------------
+// Exercise 1 - Console and constiables
+
+const firstName = "Anders";
+const lastName = "Latif";
+// EXERCISE
+// show in the console
+// My first name is Anders and my last name is Latif
+console.log("My first name is " + firstName + "and my lastname is " + lastName);
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2023";
+const increment = 1;
+
+// Add the year plus the increment
+// The result should be 2024
+// You cannot touch line 1 or 2
+
+console.log(parseInt(2023) + 1);
+
+// --------------------------------------
