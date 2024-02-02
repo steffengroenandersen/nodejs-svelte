1ff9e8143947dc181d2fd6eaef9fd01a41fbd165
steffengroenandersen <steffengroenandersen@gmail.com>
2024-02-02 10:36:10 +0100

    Add strings.js

--- 01._Miscellaneous/01._Assignments/01._Introduction/basics.js
+++ 01._Miscellaneous/01._Assignments/01._Introduction/basics.js
@@ -8,7 +8,8 @@ const lastName = "Latif";
 // EXERCISE
 // show in the console
 // My first name is Anders and my last name is Latif
-console.log("My first name is " + firstName + "and my lastname is " + lastName);
+const answer1 = "My first name is " + firstName + " and my lastname is " + lastName;
+console.log(answer1);
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -20,6 +21,7 @@ const increment = 1;
 // The result should be 2024
 // You cannot touch line 1 or 2
 
-console.log(parseInt(2023) + 1);
+answer2 = parseInt(2023) + 1;
+console.log(answer2);
 
 // --------------------------------------
---
+++ 01._Miscellaneous/01._Assignments/01._Introduction/strings.js
@@ -0,0 +1,61 @@
+// --------------------------------------
+// Exercise 3 - Add numbers from string to float
+
+const numberOne = "1.10";
+const numberTwo = "2.30";
+
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+
+const newNumber = parseFloat(numberOne) + parseFloat(numberTwo);
+
+console.log(newNumber);
+
+// --------------------------------------
+
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+
+const anotherNumberOne = "1.10";
+const anotherNumberTwo = "2.30";
+
+const anotherNewNumber = parseFloat(numberOne) + parseFloat(numberTwo);
+
+console.log(newNumber.toFixed(2));
+
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+const one = 10;
+const two = 45;
+const three = 98;
+
+// Show in the console the avg. with 5 decimals
+
+const averageNumber = ((one + two + three) / 3).toFixed(5);
+
+console.log(averageNumber);
+
+// --------------------------------------
+// Exercise 6 - Get the character by index
+
+const letters = "abc";
+
+// Get me the character "c"
+
+const c = letters[2];
+
+console.log(c);
+
+// --------------------------------------
+// Exercise 7 - Replace
+
+const fact = "You are learning javascript!";
+
+// capitalize the J in Javascript
+
+const upperCasefact = fact.toUpperCase();
+
+console.log(upperCasefact);
+
+// --------------------------------------
