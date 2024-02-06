933ca8edcc50abdf6035285c0624351ff0d170ee
steffengroenandersen <steffengroenandersen@gmail.com>
2024-02-06 09:56:55 +0100

    Finish 3_objects exercise

--- 01._Miscellaneous/01._Assignments/01._Introduction/3_objects.js
+++ 01._Miscellaneous/01._Assignments/01._Introduction/3_objects.js
@@ -7,17 +7,28 @@ const myObject = { message: "Hello, earthling! I bring peace." };
 
 // Log the message
 
+const message = myObject.message;
+console.log(message);
+
 // --------------------------------------
 // Exercise 2 - Defining an object.
 
 // Create an object that has your name and age.
 
+const user = {
+  name: "Steffen",
+  age: "32",
+};
+
 // --------------------------------------
 // Exercise 3 - Add a key-value pair
 
 const stackOverflow = {};
+stackOverflow.key = "value";
 
 // make a rule called isAllowed and let the value be true
+
+stackOverflow.isAllowed = true;
 
 // --------------------------------------
 // Exercise 4 - Remove a property
@@ -25,5 +36,9 @@ const stackOverflow = {};
 const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute."
+
+delete thisSong.description;
+thisSong.about = "Just a  tribute.";
+thisSong.songwriter = "Tenacious D.";
 
 // --------------------------------------
