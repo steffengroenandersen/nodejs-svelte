2f5ba8ed040f6903507d2a58f2036b1e5b603989
steffengroenandersen <steffengroenandersen@gmail.com>
2024-02-16 09:16:50 +0100

    Add loops.js

---
+++ 01._Miscellaneous/loops.js
@@ -0,0 +1,40 @@
+// Loop methods
+// forEach, map, filter, sort, find, reduce
+
+const dogs = [
+  { name: "Lassie", fameLevel: 13 },
+  { name: "Beethoven", fameLevel: 8 },
+  { name: "Hachiko", fameLevel: 18 },
+  { name: "Balto", fameLevel: 5 },
+];
+
+const increasedPublicityDogs = dogs.map((dog) => {
+  dog.fameLevel += 3;
+  return dog;
+});
+
+//console.log(increasedPublicityDogs);
+
+const dogsWithSex = dogs.map((dog) => ({
+  isFemale: dog.name === "Lassie" ? false : true,
+  ...dog,
+}));
+
+console.log(dogs);
+console.log(dogsWithSex);
+
+const numbers = [1, 2, 3, 4, 5];
+
+const doubledNumbers = numbers.map((number) => number * 2);
+
+//console.log(doubledNumbers);
+
+const numberObjects = [{ number: 1 }, { number: 2 }];
+
+const doubledNumberObjects = numberObjects.map((number, index, originalArray) => {
+  number.number *= 2;
+  return number;
+});
+
+//console.log(numberObjects);
+//console.log(doubledNumberObjects);
