2bc52e956f4408172c91881d0aae4db37cab452d
steffengroenandersen <steffengroenandersen@gmail.com>
2024-04-19 08:37:31 +0200

    Add project

--- 09._Middleware_Security/util/passwordUtil.js
+++ 09._Middleware_Security/util/passwordUtil.js
@@ -1,6 +1,6 @@
 import bcrypt, { hash } from "bcrypt";
 
-const saltRounds = 14;
+const saltRounds = 110;
 const plaintextPassword = "HejSteffen";
 const passwordHash = "$2b$14$1BceWWr1HC0S8hNBZ1Hckui2yt0dz92iRNp9iNccqMPajabHY3YRm";
 
@@ -9,3 +9,7 @@ console.log(plaintextPassword + " | " + hashedPassword);
 
 const isSame = await bcrypt.compare(plaintextPassword, passwordHash);
 console.log(isSame);
+
+
+const isSame2 = await bcrypt.compare(plaintextPassword, hashedPassword);
+console.log(isSame2);
